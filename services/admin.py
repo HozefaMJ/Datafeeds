20ecf0933efafa4cdf811cf64dbaf1a8b26c73cb
Hozefa <hozefa24imp@gmail.com>
2023-10-11 21:38:44 +0530

    3rd

--- services/admin.py
+++ services/admin.py
@@ -9,9 +9,21 @@ class PolicyAdmin(admin.ModelAdmin):
     list_filter = ('provider','product')
     search_fields = ['policy_number','provider','product']
 
+@admin.register(models.MappedPolicyModel)
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'valuation_date', 'broker_id', 'policy_number','financial_account_name','client_name','holding_name', 'holding_market_value_holding_currency','holding_currency','units','unit_price','total_contribution','book_cost','product','db_entry_date')
+    list_filter = ('provider','product')
+    search_fields = ['policy_number','provider','product']
+
 @admin.register(models.FinancialAccountModel)
 class FinancialAdmin(admin.ModelAdmin):
     list_display = ('provider', 'valuation_date', 'policy_number')
+    list_filter = ('provider','product')
+    search_fields = ['policy_number','provider','product']
+
+@admin.register(models.MappedFinancialAccountModel)
+class FinancialAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'valuation_date', 'policy_number','financial_account_name','client_name')
     list_filter = ('provider','product')
     search_fields = ['policy_number','provider','product']
 
--- services/utils.py
+++ services/utils.py
@@ -81,6 +81,90 @@ import pysftp
 from urllib.parse import urlparse
 
 
+# def collect_attachments(email_address, subject_name, client_id, client_secret, tenant_id):
+#     # Get today's date
+#     today = datetime.date.today()
+#     today_iso_format = today.isoformat()
+
+#     # Authenticate and obtain an access token
+#     token_url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
+#     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+#     data = {
+#         'grant_type': 'client_credentials',
+#         'client_id': client_id,
+#         'client_secret': client_secret,
+#         'scope': 'https://graph.microsoft.com/.default'
+#     }
+
+#     response = requests.post(token_url, headers=headers, data=data)
+#     access_token = response.json()['access_token']
+
+#     # Fetch emails with the specific subject received today
+#     api_url = f'https://graph.microsoft.com/v1.0/users/{email_address}/messages'
+#     headers = {
+#         'Authorization': f'Bearer {access_token}',
+#         'Content-Type': 'application/json'
+#     }
+
+#     params = {
+#         '$filter': f"subject eq '{subject_name}' and receivedDateTime ge {today_iso_format}",
+#         '$select': 'id'
+#     }
+
+#     response = requests.get(api_url, headers=headers, params=params)
+#     emails = response.json()['value']
+
+#     # Create S3 storage instance
+#     s3_storage = S3Boto3Storage()
+
+#     # Iterate over emails and retrieve attachments
+#     for email in emails:
+#         email_id = email['id']
+#         attachment_url = f'{api_url}/{email_id}/attachments'
+
+#         response = requests.get(attachment_url, headers=headers)
+#         attachments = response.json()['value']
+
+#         # Download attachments with specific file extensions
+#         for attachment in attachments:
+#             attachment_id = attachment['id']
+#             attachment_name = attachment['name']
+#             attachment_url = f'{attachment_url}/{attachment_id}/$value'
+
+#             # Check if the attachment has a supported file extension
+#             if attachment_name.endswith(('.xlsx', '.xls', '.csv')):
+#                 response = requests.get(attachment_url, headers=headers)
+
+#                 # Save attachment to S3 bucket
+#                 file_content = ContentFile(response.content)
+#                 s3_file_path = f'records/{attachment_name}'  # S3 file path
+#                 s3_storage.save(s3_file_path, file_content)
+
+#                 # Create a new record in the Django model
+#                 record = Records.objects.create(
+#                     provider_name=subject_name,
+#                     file=s3_storage.url(s3_file_path),
+#                     date=today
+#                 )
+#                 record.save()
+
+#                 # Get the S3 file URL
+#                 s3_file_url = s3_storage.url(s3_file_path)
+#                 df =  pd.read_csv(s3_file_url,encoding = "ISO-8859-1")
+#                 print('COLUMNS::::',df.columns)
+
+#                 print(f'Saved attachment: {attachment_name}')
+#                 print(f'S3 file URL: {s3_file_url}')
+
+#                 # Return the S3 file URL
+#                 return s3_file_url
+
+import datetime
+import requests
+from django.core.files.base import ContentFile
+from services.models import Records  # Assuming 'records' is the Django app name
+
 def collect_attachments(email_address, subject_name, client_id, client_secret, tenant_id):
     # Get today's date
     today = datetime.date.today()
@@ -136,29 +220,23 @@ def collect_attachments(email_address, subject_name, client_id, client_secret, t
             if attachment_name.endswith(('.xlsx', '.xls', '.csv')):
                 response = requests.get(attachment_url, headers=headers)
 
-                # Save attachment to S3 bucket
-                file_content = ContentFile(response.content)
-                s3_file_path = f'records/{attachment_name}'  # S3 file path
-                s3_storage.save(s3_file_path, file_content)
+                # Get the attachment content
+                attachment_content = response.content
 
-                # Create a new record in the Django model
+                # Create a new record in the Django model (optional)
                 record = Records.objects.create(
                     provider_name=subject_name,
-                    file=s3_storage.url(s3_file_path),
+                    file=attachment_name,  # Change this to save the file name
                     date=today
                 )
                 record.save()
 
-                # Get the S3 file URL
-                s3_file_url = s3_storage.url(s3_file_path)
-                df =  pd.read_csv(s3_file_url,encoding = "ISO-8859-1")
-                print('COLUMNS::::',df.columns)
+                # print(f'Saved attachment: {attachment_name}')
+                # print(f'Attachment content length: {len(attachment_content)}')
 
-                print(f'Saved attachment: {attachment_name}')
-                print(f'S3 file URL: {s3_file_url}')
+                # Return the attachment content
+                return attachment_content
 
-                # Return the S3 file URL
-                return s3_file_url
 
 import os
 
